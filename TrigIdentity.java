39501088cf3151a863d2788f1b9c9e7c2d59f9c2
Keaton Stwalley <23keatonstwalley@usd266.com>
2023-01-10 11:52:32 -0600

    added rounding to answer

--- TrigIdentity.java
+++ TrigIdentity.java
@@ -2,7 +2,16 @@ public class TrigIdentity {
     public static void main(String[] args) {
         double theta = Double.parseDouble(args[0]);
         double result = (Math.pow(Math.sin(Math.toRadians(theta)),2)) + (Math.pow(Math.cos(Math.toRadians(theta)),2));
-        System.out.println(result);
+        /*
+        
+        other method of rounding answer before I knew the round method
+        double error = 0.00001;
+
+        if (Math.abs(result-1)<error){
+            result = 1;
+        }
+        */
+        System.out.println("Sin^2("+args[0]+") + Cos^2("+args[0]+") = "+Math.round(result));
 
 
     }
